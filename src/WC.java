68d0351a68fd69449224adc02e0a61a780d6fb17
Akash Sharma <akash.sharma@igniteconnex.com>
2024-05-13 01:16:44 +0530

    Added WC(wrapper class) class demonstrating the usage of wrapper classes and their conversion methods

---
+++ src/WC.java
@@ -0,0 +1,17 @@
+public class WC {
+    public static void main(String[] args) {
+        Integer a = Integer.valueOf("101011",2);
+        Integer b = Integer.valueOf("123");
+        Double c =  Double.valueOf(3.14);
+        int d = Integer.parseInt("456");
+        double e = Double.parseDouble("13.45");
+        int var = a.intValue();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(var);
+    }
+}
